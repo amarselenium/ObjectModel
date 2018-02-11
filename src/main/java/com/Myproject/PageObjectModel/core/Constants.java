5356b82e4a000c0563e8226e9b40c76dd15d537c
Amaresh <30757367+amarselenium@users.noreply.github.com>
2018-02-11 13:05:18 +0530

    Update Constants.java
    constant changed

--- src/main/java/com/Myproject/PageObjectModel/core/Constants.java
+++ src/main/java/com/Myproject/PageObjectModel/core/Constants.java
@@ -5,6 +5,7 @@ public class Constants {
 		public static String username = "txtUsername";
 		public static String password = "txtPassword";
 		public static String login_btn= "btnLogin";
+	        public static String logout_btn= "btnLogout";
 		
 
 }
