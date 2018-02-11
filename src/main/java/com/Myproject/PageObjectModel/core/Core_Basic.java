7daeded7508caee44472ebfd91c68f16c57d0ed0
Amaresh <Amaresh@192.168.0.4>
2018-02-11 19:48:42 +0530

    commiting code for junnit 4

--- src/main/java/com/Myproject/PageObjectModel/core/Core_Basic.java
+++ src/main/java/com/Myproject/PageObjectModel/core/Core_Basic.java
@@ -3,8 +3,6 @@ package com.Myproject.PageObjectModel.core;
 import java.io.IOException;
 
 import org.apache.log4j.Logger;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeTest;
